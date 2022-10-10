e23826047c83d2386c3c45ba7555eedc33e8b166
ethanm123 <ethanmansbridge1@gmail.com>
2022-10-10 16:59:44 +0900

    Added apiCalls template, added crUpdate, modified manifest accordingly and updated size on icon

---
+++ 9animeUpdate.js
@@ -0,0 +1,1 @@
+import { getAnimeID, updateAnimeCount } from "./apiCalls";
---
+++ apiCalls.js
@@ -0,0 +1,13 @@
+const axios = require("axios");
+
+export function getAnimeID(animeName) {
+    console.log('hi');
+}
+
+export function updateAnimeCount(id, episode) {
+    console.log('hi');
+}
+
+function setCompleted(id) {
+    console.log('hi');
+}
